8c96bab42dd389ae8b9994ea54009250d6221dde
PaulienBuskens <32159110+PaulienBuskens@users.noreply.github.com>
2019-01-04 15:41:40 +0100

    scroll bug fixed

--- js/app.js
+++ js/app.js
@@ -6,18 +6,3 @@ scrollnav.init(content, {
     insertLocation: 'append'
 });
 
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("up").style.display = "block";
-  } else {
-    document.getElementById("up").style.display = "none";
-  }
-  
-}
-
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0; 
-}
